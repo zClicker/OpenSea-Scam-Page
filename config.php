43914a9c8b423fbc080f1fbb028cc42ce9f4ac1d
TheDotNet1 <98549794+TheDotNet1@users.noreply.github.com>
2023-01-17 14:03:23 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php 
+
+$host = 'interconnected-server.com';  // the domain name .com without https://
+
+$username = 'info@interconnected-server.com'; //username of the email account
+
+$password = '#3344uy365.com'; //password of the email account
+
+$setForm = 'info@interconnected-server.com'; //email that is mail is sending from (same as the username);
+
+
+?>
